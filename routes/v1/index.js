924310feddc51046b229b38c1201a20e417bc085
David Thayer <dthayer16@gmail.com>
2019-09-18 14:23:45 -0700

    removing variables from route URL query.

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 
 router.get("/events", function (req, res) {
 
-    const url = "http://api.eventful.com/json/events/search?app_key=" + process.env.eventfulKey + "&location=" + req.body.location + "&date=Future&category=music,comedy,family_fun_kids,festivals_parades,movies_film,food,art,holiday,attractions,community,singles_social,outdoors_recreation,performing_arts,animals,sports,other";
+    const url = "http://api.eventful.com/json/events/search?app_key=dmcR44C3PdqNC2Dh&location=San+Diego&date=Future&category=music,comedy,family_fun_kids,festivals_parades,movies_film,food,art,holiday,attractions,community,singles_social,outdoors_recreation,performing_arts,animals,sports,other";
 
     axios.get(url)
         .then(response => {
