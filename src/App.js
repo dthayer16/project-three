cbe0fdcd40cb0d95c171ecb9a945caa5901d11a0
David Thayer <dthayer16@gmail.com>
2019-09-18 10:05:49 -0700

    editing page names and links. adding server for API calls to protect API Keys.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
 import About from "./pages/About";
-import Search from "./pages/Search";
+import StuffToDo from "./pages/StuffToDo";
+import TravelDetails from "./pages/TravelDetails";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -15,8 +15,8 @@ function App() {
         <Wrapper>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
+          <Route exact path="/discover" component={StuffToDo} />
+          <Route exact path="/search" component={TravelDetails} />
         </Wrapper>
         <Footer />
       </div>
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,11 @@
 import axios from "axios";
+require("dotenv").config();
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
+// Export an object containing methods we'll use for accessing the eventful API
 
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
-  },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
+    getEvents: function(location) {
+    return axios.get("http://api.eventful.com/json/events/search?app_key=" + process.env.eventfulKey + "&location=" + location + "date=Future&sort_order=date&category=music,comedy,family_fun_kids,festivals_parades,movies_film,food,art,holiday,attractions,community,singles_social,outdoors_recreation,performing_arts,animals,sports,other");
   }
 };
