7af4895b3365667edefdc5b50c058e0e2911a1fd
David Thayer <dthayer16@gmail.com>
2019-09-25 14:51:01 -0700

    logged in as, initial testing.

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Discover from "./pages/Discover";
 import About from "./pages/About";
